bf4c7c55b654174041bd8270950a9db994139bcd
Marissa Willison <canuck687@gmail.com>
2022-01-20 10:50:43 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,21 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", 'Amy Android', 'Robo Trumble']; 
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(){
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//      * Fight all enemy-robots
+//      * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+// console.log(playerName, playerAttack, playerHealth);
+
+
+
+var fight = function(enemyName){
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -58,11 +66,15 @@ var fight = function(){
     } else {
         window.alert('You need to choose a valid option. Try again!');
     }
-
-
-
-
-
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+    }
+
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] = ' is at ' + i + ' index');
+// }
+
